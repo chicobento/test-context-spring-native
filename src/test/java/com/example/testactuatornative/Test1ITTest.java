da99110d7773f9558a51a11d392c157aab10d911
Francisco Bento <chicobento@gmail.com>
2023-08-15 20:15:20 +0000

    Add cloud-context and logback.xml

--- src/test/java/com/example/testactuatornative/Test1ITTest.java
+++ src/test/java/com/example/testactuatornative/Test1ITTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = { "management.server.port=0", "value1=value1" })
+@SpringBootTest(properties = { "value1=value1" })
 @SuppressWarnings("unchecked")
-@ActiveProfiles("actuator")
 public class Test1ITTest {
 
 	@Test
--- src/test/java/com/example/testactuatornative/Test2ITTest.java
+++ src/test/java/com/example/testactuatornative/Test2ITTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = { "management.server.port=0", "value2=value2" })
+@SpringBootTest(properties = { "value2=value2" })
 @SuppressWarnings("unchecked")
-@ActiveProfiles("actuator")
 public class Test2ITTest {
 
 	@Test
